69c5e65b41c4be15923ee1b09e49b5f78b85dd1c
David England <david.england.de@googlemail.com>
2024-10-22 07:26:09 +0100

    Stub DB layer

---
+++ AccountTrackerDB/RepositoryContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AccountTrackerDB
+{
+	public class RepositoryContext : DbContext
+	{
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			base.OnConfiguring(optionsBuilder);
+		}
+	}
+}
